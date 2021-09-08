298e5c22729a5872471e788b8dfbc3814da1b119
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-09-08 13:42:00 +0200

    add increment test in model Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'comment association' do
+    it 'should increment comments_count' do
+      post = Post.new(name: 'Hello')
+      post.comments << Comment.new(content: 'Test')
+      post.save
+      expect(Comment.count).to eq(1)
+      expect(post.comments_count).to eq(1)
+    end
+  end
 end
