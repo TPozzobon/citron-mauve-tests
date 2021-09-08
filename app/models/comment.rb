d149fe7446ad7b53ae01f02583dcce811bb236b4
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-09-08 14:30:18 +0200

    install gem factory girl

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post, counter_cache: true #entraînera moins de demandes à la base de données (perfs up)
+
+  validates :content, length: {minimum: 10}
 end
