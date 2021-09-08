17db3e7b799cdc2e28cb927f3356ef06837f17fa
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-09-08 11:14:55 +0200

    create index method in users controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,3 @@ class PagesController < ApplicationController
   def home
   end
 end
-
-# def index
-#   array = ["Keanu", "Morpheus", "Trinity"]
-
-#   respond_to do |format|
-#     format.html { render html: "Strongly Matrix 4" }
-#     format.json { render json: array }
-#   end
-# end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    array = ["Keanu", "Morpheus", "Trinity"]
+  
+    respond_to do |format|
+      format.html { render html: "Strongly Matrix 4" }
+      format.json { render json: array }
+    end
+  end
 end
